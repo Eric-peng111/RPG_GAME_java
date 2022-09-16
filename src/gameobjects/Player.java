5bcd47607312c8354066a4edfbb8ee5c322df2ee
Sijie Jack Fan <u7253519@anu.edu.au>
2022-09-16 23:29:33 +1000

    Revert "Implement Weapons to gameobjects"
    This reverts commit b1bbb6950acc450c823fd6247f0904f8e127475d

--- src/gameobjects/Player.java
+++ src/gameobjects/Player.java
@@ -10,9 +10,8 @@ public class Player extends Actor{
 
     private static Player single_instance = null;
     private int maxHp, hp, attack, level,xp,gold;
-    private String weapon;
     private ArrayList<Thing> bag;
-    private Player(String aName, String aDescription, Room aRoom,int maxHp, int attack, int xp, int gold, int level, String weapon) {
+    private Player(String aName, String aDescription, Room aRoom,int maxHp, int attack, int xp, int gold, int level) {
         super(aName, aDescription, aRoom);
         this.maxHp = maxHp;
         this.hp = maxHp;
@@ -20,7 +19,6 @@ public class Player extends Actor{
         this.xp = xp;
         this.gold = gold;
         this.level = level;
-        this.weapon = weapon;
         this.bag = new ArrayList<Thing>();
     }
 
@@ -28,7 +26,7 @@ public class Player extends Actor{
     {
         Room startRoom = new Room("town", "you have returned to town", 1, 2, 3, 4, 5, Direction.NOEXIT);
         if (single_instance == null)
-            single_instance = new Player("player","player in the game", startRoom,1100, 300, 200, 0, 1, "Iron Sword");
+            single_instance = new Player("player","player in the game", startRoom,1100, 300, 200, 0, 1);
 
         return single_instance;
     }
