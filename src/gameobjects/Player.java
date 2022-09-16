7c33f5c4eb2d823e95ef41fcfb8847dd63cea748
u7306766 <u7306766@anu.edu.au>
2022-09-16 13:05:17 +1000

    add getWp in player class to get player's weapon

--- src/gameobjects/Player.java
+++ src/gameobjects/Player.java
@@ -10,7 +10,7 @@ public class Player extends Actor{
 
     private static Player single_instance = null;
     private int maxHp, hp, attack, level,xp,gold;
-    private Weapons weapon;
+    private Weapons wp;
     private ArrayList<Thing> bag;
     private Player(String aName, String aDescription, Room aRoom,int maxHp, int attack, int xp, int gold, int level, Weapons weapon) {
         super(aName, aDescription, aRoom);
@@ -20,8 +20,15 @@ public class Player extends Actor{
         this.xp = xp;
         this.gold = gold;
         this.level = level;
-        this.weapon = weapon;
         this.bag = new ArrayList<Thing>();
+        wp = weapon;
+    }
+    public Weapons getWp() {
+        return wp;
+    }
+
+    public void setWp(Weapons weapon) {
+        wp = weapon;
     }
 
     public static Player getInstance()
@@ -29,7 +36,7 @@ public class Player extends Actor{
         Weapons sword = new Weapons("Iron Sword", "A trusty sword", 100);
         Room startRoom = new Room("town", "you have returned to town", 1, 2, 3, 4, 5, Direction.NOEXIT, Direction.NOEXIT, Direction.NOEXIT);
         if (single_instance == null)
-            single_instance = new Player("player","player in the game", startRoom,1100, 300 + sword.getDmg(), 200, 0, 1, sword);
+            single_instance = new Player("player","player in the game", startRoom,1100, 300 + sword.getDmg() , 200, 0, 1, sword);
 
         return single_instance;
     }
